2383025ec00b09000fdf5489774edb235eb4aa2c
Melissa <melissa.pegas@hotmail.fr>
2024-12-05 14:05:24 +0100

    changes to script -1

--- script/scriptMelissa.js
+++ script/scriptMelissa.js
@@ -1,25 +1,5 @@
 
 try {
-    fetch('https://jsonplaceholder.typicode.com/posts/[postId]/comments', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
-} catch(error) {
-  console.log(error);
-}
-
-
-try {
     fetch('https://jsonplaceholder.typicode.com/users/[userId]', {
     method: 'POST',
     body: JSON.stringify({
@@ -34,13 +14,15 @@ try {
   }).then((response) => response.json())
   .then((json) => console.log(json));
 
+  //va chercher username
+  let userElement = document.createElement("userInfo")
+  userElement.textContent = `username: ${user.username}`
+  
 } catch(error) {
   console.log(error);
 }
-
-
 try {
-    fetch('https://jsonplaceholder.typicode.com/posts?userId=[userId]', {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
      title: 'foo',
@@ -54,8 +36,17 @@ try {
   }).then((response) => response.json())
   .then((json) => console.log(json));
 
+  //va chercher commentaire
+  
+
+  let userElement = document.createElement("userInfo")
+  userElement.textContent = `commentaire: ${user.body}`
+  
 } catch(error) {
   console.log(error);
 }
+
+let user = userArray[1];
+        console.log(user);
 
 
