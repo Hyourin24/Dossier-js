b2dc87fa43426a8f524ccb53738cf1d2eef1a0d9
Melissa <melissa.pegas@hotmail.fr>
2024-12-05 15:13:36 +0100

    changes to script -2

--- script/scriptMelissa.js
+++ script/scriptMelissa.js
@@ -1,27 +1,5 @@
 
 try {
-    fetch('https://jsonplaceholder.typicode.com/users/[userId]', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
-  //va chercher username
-  let userElement = document.createElement("userInfo")
-  userElement.textContent = `username: ${user.username}`
-  
-} catch(error) {
-  console.log(error);
-}
-try {
     fetch('https://jsonplaceholder.typicode.com/posts', {
     method: 'POST',
     body: JSON.stringify({
@@ -36,17 +14,52 @@ try {
   }).then((response) => response.json())
   .then((json) => console.log(json));
 
-  //va chercher commentaire
-  
+fetch('https://jsonplaceholder.typicode.com/users/[userId]', {
+    method: 'POST',
+    body: JSON.stringify({
+     title: 'foo',
+      body: 'bar',
+      userId: 1,
+  }),
+   headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+   },
+         
+  }).then((response) => response.json())
+  .then((json) => console.log(json));
 
-  let userElement = document.createElement("userInfo")
-  userElement.textContent = `commentaire: ${user.body}`
-  
+
+
+
 } catch(error) {
   console.log(error);
 }
 
-let user = userArray[1];
-        console.log(user);
+async function AfficherPost() {
+    try {
+        let responsePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
+        let posts = await responsePosts.json();
 
+        let responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
+        let users = await responseUsers.json();
 
+        posts.forEach(post => {
+            let userSection = document.createElement("postDiv");
+            userSection.classList.add("postInfo");
+            userSection.textContent = `Titre: ${post.title}, Contenu: ${post.body}`;
+            document.body.appendChild(userSection);
+
+            let user = users.find(user => user.id === post.userId);
+            if (user) {
+                let userSection = document.createElement("div");
+                userSection.classList.add("userInfo");
+                userSection.textContent = `Nom de l'utilisateur: ${user.name}`;
+                userSection.appendChild(userInfoDiv);
+            }
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+AfficherPost();
