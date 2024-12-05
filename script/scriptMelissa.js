e0a57c91bd05b620bdf21950a204f022bbed7c70
Melissa <melissa.pegas@hotmail.fr>
2024-12-05 13:43:29 +0100

    added delete button script

---
+++ script/scriptMelissa.js
@@ -0,0 +1,61 @@
+
+try {
+    fetch('https://jsonplaceholder.typicode.com/posts/[postId]/comments', {
+    method: 'POST',
+    body: JSON.stringify({
+     title: 'foo',
+      body: 'bar',
+      userId: 1,
+  }),
+   headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+   },
+         
+  }).then((response) => response.json())
+  .then((json) => console.log(json));
+
+} catch(error) {
+  console.log(error);
+}
+
+
+try {
+    fetch('https://jsonplaceholder.typicode.com/users/[userId]', {
+    method: 'POST',
+    body: JSON.stringify({
+     title: 'foo',
+      body: 'bar',
+      userId: 1,
+  }),
+   headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+   },
+         
+  }).then((response) => response.json())
+  .then((json) => console.log(json));
+
+} catch(error) {
+  console.log(error);
+}
+
+
+try {
+    fetch('https://jsonplaceholder.typicode.com/posts?userId=[userId]', {
+    method: 'POST',
+    body: JSON.stringify({
+     title: 'foo',
+      body: 'bar',
+      userId: 1,
+  }),
+   headers: {
+  'Content-type': 'application/json; charset=UTF-8',
+   },
+         
+  }).then((response) => response.json())
+  .then((json) => console.log(json));
+
+} catch(error) {
+  console.log(error);
+}
+
+
