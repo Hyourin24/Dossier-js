8b421db872c2fe2fa8697af437db4a11ae57adcb
Melissa <melissa.pegas@hotmail.fr>
2024-12-06 00:14:29 +0100

    changes to css -1

--- script/scriptMelissa.js
+++ script/scriptMelissa.js
@@ -1,50 +1,17 @@
 
-try {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
- fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
-
-
-
-} catch(error) {
-  console.log(error);
-}
-
 let main = document.querySelector(".main");
-let userSection3 = document.querySelector(".userSection3")
-async function AfficherPost() {
+ export let userSection3 = document.querySelector(".userSection3")
+  async function AfficherPost() {
     try {
         let responsePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
         let posts = await responsePosts.json();
 
         let responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
         let users = await responseUsers.json();
-
+        
+        let responseComments = await fetch('https://jsonplaceholder.typicode.com/comments');
+        let comments = await responseComments.json(); 
+       
         posts.forEach(post => {
             let postDiv = document.createElement("div");
             postDiv.classList.add("postInfo");
@@ -52,10 +19,16 @@ async function AfficherPost() {
             userSection3.appendChild(postDiv);
 
             let user = users.find(user => user.id === post.userId);
-                let userInfoDiv = document.createElement("div");
-                userInfoDiv.classList.add("userInfo");
-                userInfoDiv.textContent = `Nom de l'utilisateur: ${user.name}`;
-                postDiv.appendChild(userInfoDiv);
+            let userInfoDiv = document.createElement("div");
+            userInfoDiv.classList.add("userInfo");
+            userInfoDiv.textContent = `Nom de l'utilisateur: ${user.name}`;
+            postDiv.appendChild(userInfoDiv);
+
+            let comment = users.find( comment => comment.id === post.userId);
+            let userCommentsDiv = document.createElement("div");
+            userCommentsDiv.classList.add("userComments");
+            userCommentsDiv.textContent = `Commentaire: ${comments.body}`;
+            userSection3.appendChild(userCommentsDiv);
         });
     } catch (error) {
         console.error(error);
@@ -63,3 +36,4 @@ async function AfficherPost() {
 }
 
 AfficherPost();
+
