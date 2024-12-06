81dec0268afa300977d1279592ae72cb3fee2422
Melissa <melissa.pegas@hotmail.fr>
2024-12-06 12:21:46 +0100

    changes to button -1

--- script/boutonSuppr.js
+++ script/boutonSuppr.js
@@ -1,4 +1,11 @@
+
+import { userSection3 } from "./scriptMelissa.js"
+
+let monBouton = document.querySelector(".monBouton");
 monBouton.addEventListener("click", function () {
-    document.body.removeChild(userInfo);
-    nouveauParagraphe.remove();
+    userSection3.forEach(post => {
+        userSection3.remove();
+    });
+
+
 });
--- script/scriptMelissa.js
+++ script/scriptMelissa.js
@@ -1,49 +1,17 @@
 
-
-try {
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
- fetch('https://jsonplaceholder.typicode.com/users', {
-    method: 'POST',
-    body: JSON.stringify({
-     title: 'foo',
-      body: 'bar',
-      userId: 1,
-  }),
-   headers: {
-  'Content-type': 'application/json; charset=UTF-8',
-   },
-         
-  }).then((response) => response.json())
-  .then((json) => console.log(json));
-
-
-
-
-} catch(error) {
-  console.log(error);
-}
-
-async function AfficherPost() {
+let main = document.querySelector(".main");
+ export let userSection3 = document.querySelector(".userSection3")
+  async function AfficherPost() {
     try {
         let responsePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
         let posts = await responsePosts.json();
 
         let responseUsers = await fetch('https://jsonplaceholder.typicode.com/users');
         let users = await responseUsers.json();
-
+        
+        let responseComments = await fetch('https://jsonplaceholder.typicode.com/comments');
+        let comments = await responseComments.json(); 
+       
         posts.forEach(post => {
             let userSection = document.createElement("postDiv");
             userSection.classList.add("postInfo");
@@ -51,16 +19,25 @@ async function AfficherPost() {
             document.body.appendChild(userSection);
 
             let user = users.find(user => user.id === post.userId);
-            if (user) {
-                let userSection = document.createElement("div");
-                userSection.classList.add("userInfo");
-                userSection.textContent = `Nom de l'utilisateur: ${user.name}`;
-                userSection.appendChild(userInfoDiv);
-            }
+            let userInfoDiv = document.createElement("div");
+            userInfoDiv.classList.add("userInfo");
+            userInfoDiv.textContent = `Nom de l'utilisateur: ${user.name}`;
+            postDiv.appendChild(userInfoDiv);
+
+            let comment = users.find( comment => comment.id === post.userId);
+            let userCommentsDiv = document.createElement("div");
+            userCommentsDiv.classList.add("userComments");
+            userCommentsDiv.textContent = `Commentaire: ${comments.body}`;
+            userSection3.appendChild(userCommentsDiv);
         });
+
+
+
+
     } catch (error) {
         console.error(error);
     }
 }
 
 AfficherPost();
+
