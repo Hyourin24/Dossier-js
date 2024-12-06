bbb90170efbb3f473c665e06f6d0c3a0d5d043b8
Melissa <melissa.pegas@hotmail.fr>
2024-12-06 12:21:46 +0100

    changes to button -1

--- script/boutonSuppr.js
+++ script/boutonSuppr.js
@@ -1,11 +1,5 @@
 
-import { userSection3 } from "./scriptMelissa.js"
-
-let monBouton = document.querySelector(".monBouton");
-monBouton.addEventListener("click", function () {
-    userSection3.forEach(post => {
-        userSection3.remove();
-    });
 
 
-});
+
+
