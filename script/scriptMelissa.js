70beba8ff8b32746a2b10adead3bad45739d6742
Adrien <adrienribeiro999@gmail.com>
2024-12-09 12:18:57 +0100

    SAVE

--- script/scriptMelissa.js
+++ script/scriptMelissa.js
@@ -35,7 +35,7 @@ try {
 } catch(error) {
   console.log(error);
 }
-
+let userSection3 = document.querySelector("userSection3")
 async function AfficherPost() {
     try {
         let responsePosts = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -45,10 +45,10 @@ async function AfficherPost() {
         let users = await responseUsers.json();
 
         posts.forEach(post => {
-            let userSection = document.createElement("postDiv");
-            userSection.classList.add("postInfo");
-            userSection.textContent = `Titre: ${post.title}, Contenu: ${post.body}`;
-            document.body.appendChild(userSection);
+            let postDiv = document.createElement("div");
+            postDiv.classList.add("postInfo");
+            userSection3.appendChild(postDiv);
+            postDiv.textContent = `Titre: ${post.title}, Contenu: ${post.body}`;
 
             let user = users.find(user => user.id === post.userId);
             if (user) {
